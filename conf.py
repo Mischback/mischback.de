77ec03af92aa6d335f203f6151dc9cf6397f351a
Mischback <dev@mischback.de>
2022-11-25 15:32:59 +0100

    Create a minimal sphinx setup

---
+++ conf.py
@@ -0,0 +1,16 @@
+"""Configuration file for Sphinx."""
+
+# Python imports
+import datetime
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "mischback.de"
+author = "Mischback"
+copyright = "{}, {}".format(datetime.datetime.now().year, author)
