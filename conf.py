ecbace1c3c6113478c1cf1a92371dfc376a6f471
Mischback <dev@mischback.de>
2022-11-26 11:10:12 +0100

    Set sphinx's root_doc to sitemap

--- conf.py
+++ conf.py
@@ -14,3 +14,12 @@ import datetime
 project = "mischback.de"
 author = "Mischback"
 copyright = "{}, {}".format(datetime.datetime.now().year, author)
+
+# This document contains the *master TOC*.
+#
+# It is not necessarily the ``index.rst``, so we can build the actual
+# navigation logic without caring (too much) about ``sphinx``'s internal
+# toc-related logic.
+#
+# Ref: https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-root_doc
+root_doc = "sitemap"
---
+++ content/sitemap.rst
@@ -0,0 +1,11 @@
+################
+Sitemap (Humans)
+################
+
+.. toctree::
+   :caption: Table of Contents
+   :name: mastertoc
+   :glob:
+
+   *
+   development/*
