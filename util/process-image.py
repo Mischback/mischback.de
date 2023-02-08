27b60cacc89b3079555bf0ca78a05c49bdc6143a
Mischback <dev@mischback.de>
2023-02-08 19:28:39 +0100

    Create script file

---
+++ util/process-image.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Process a source image to create the required responsive versions."""
+
+
+def main():
+    """Execute the processing."""
+    pass
+
+
+if __name__ == "__main__":
+    main()
