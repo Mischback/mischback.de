8e45c32f20d0c379f466430b57c6129eec4a8721
Mischback <dev@mischback.de>
2023-02-08 20:26:23 +0100

    Add argument parsing

--- util/process-image.py
+++ util/process-image.py
@@ -3,9 +3,39 @@
 """Process a source image to create the required responsive versions."""
 
 
+# Python imports
+import argparse
+
+
+def parse_args():
+    """Parse the command line arguments.
+
+    The function sets up the ``ArgumentParser`` and returns the parsed
+    arguments for further processing.
+    """
+    parser = argparse.ArgumentParser(
+        description="Create required responsive images from source file"
+    )
+
+    # mandatory arguments
+    parser.add_argument(
+        "source_file", action="store", help="Path/name of the source file"
+    )
+    parser.add_argument(
+        "destination_directory",
+        action="store",
+        help="Path to the destination directory",
+    )
+
+    return parser.parse_args()
+
+
 def main():
     """Execute the processing."""
-    pass
+    # get the arguments
+    args = parse_args()
+
+    print("[DEBUG] args: {}".format(args))
 
 
 if __name__ == "__main__":
