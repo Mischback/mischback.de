8a0dde0396f9ca5978a5a97252b634017310a12f
Mischback <dev@mischback.de>
2024-03-28 16:12:53 +0100

    Provide argument parsing

--- util/cache-busting.py
+++ util/cache-busting.py
@@ -3,8 +3,10 @@
 """Custom script to perform cache busting for static assets."""
 
 # Python imports
+import argparse
 import logging
 import logging.config
+from pathlib import Path
 
 # get a module-level logger
 logger = logging.getLogger(__name__)
@@ -35,9 +37,43 @@ LOGGING_DEFAULT_CONFIG = {
 }
 
 
+def parse_args():
+    """Parse the command line arguments."""
+    # create the main parser
+    parser = argparse.ArgumentParser(
+        description="Perform cache busting for static assets"
+    )
+
+    parser.add_argument(
+        "--source",
+        action="store",
+        type=str,
+        required=True,
+        help="Path to the source directory",
+    )
+
+    parser.add_argument(
+        "--asset",
+        dest="assets",
+        action="append",
+        type=str,
+        required=True,
+        help="Relative path from source directory to an asset file",
+    )
+
+    return parser.parse_args()
+
+
 def main():
     """Perform the cache busting when executing the script from command line."""
-    logger.info("foo")
+    args = parse_args()
+    logger.debug("args: %r", args)
+
+    source = Path(args.source)
+    logger.debug("source: %r", source)
+
+    assets = {Path(a) for a in args.assets}
+    logger.debug("assets: %r", assets)
 
 
 if __name__ == "__main__":
